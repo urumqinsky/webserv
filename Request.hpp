31291d28a4a052e679c0c3b91cf52f54adc7352d
Hector Eula <heula@ox-h5.msk.21-school.ru>
2022-01-20 21:03:02 +0300

    Request.hpp

--- Request.hpp
+++ Request.hpp
@@ -28,21 +28,26 @@ public:
 	std::string const &getIPport();
 	void setIPport(int IPport);
 	void parseFd(std::string req);
+	friend void parseFirstLine(Request &other);
+
+
+protected:
+	std::string buf;
+	Status	status;	
+	chunkStatus	chunkStatus;
+	std::string method;
+	std::string path;
+	std::string body;
+
 
 private:
 	Request(const Request &other);
 	Request	&operator=(const Request &other);
 
 	std::string IPport; //?
-	std::string method;
-	std::string path;
-	std::string body;
-	std::string buf;
 	int errorStatus;
 
 
-	Status	status;	
-	chunkStatus	chunkStatus;
 
 	std::map <std::string, std::string> headers;
 	std::string responce;  
